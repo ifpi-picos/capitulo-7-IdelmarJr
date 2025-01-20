6d8522fb7ac86bb0519f58726d9454f7b1181908
Idelmar Jr <idelmarjuniorre@gmail.com>
2025-01-20 19:21:08 -0300

    Update ex4.js

--- ex4.js
+++ ex4.js
@@ -2,9 +2,8 @@ const produto = {
     nome: 'Whiskys Old Par',
     preco: 200,
     quantidade: 14,
-    valorTotal: function valor () {
-     return this.preco * this.quantidade
- };
+    valorTotal: function valor() {
+        return this.preco * this.quantidade
+    },
 };
-console.log('Valor Total do estoque ' + produto.valorTotal);
-
+console.log(produto.valorTotal());
